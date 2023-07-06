191b263ebc81467c5c6d4cec3de2f21b19874142
emmakotey <emmakotey@gmail.com>
2023-07-06 15:28:47 +0000

    wish

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-"""This code includes a function that performs the multiplication of a floa"""
+"""Contains a function that multiplies a float by multiplier"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    
+    """Multiplies a float by multiplier
+    Args:
+        multiplier (float): The multiplier
+    Returns:
+        A function that multiplies a float by multiplier
+    """
 
     def multiplier_func(number: float) -> float:
-        """fload is multiplied by a multiplier"""
+        """Multiplies a float by multiplier"""
         return multiplier * number
 
     return multiplier_func
