4d47a1cd576206af033dedc535977fb9610e6a5f
emmakotey <emmakotey@gmail.com>
2023-07-06 13:30:22 +0000

    wish

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""This function takes two floats 'a' and 'b' as arguments 
-    and return their sum as float
-"""
+"""This function takes two floats 'a' and 'b' and return sum as float"""
 
 
 def add(a: float, b: float) -> float:
