558e7b10f1904a56921096f6b2710bcb429b5788
emmakotey <emmakotey@gmail.com>
2023-07-06 12:52:42 +0000

    this

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""This function takes two floats 'a' and 'b' as arguments 
+    and return their sum as float
+"""
 
 def add(a: float, b: float) -> float:
     """ Add two integers together."""
